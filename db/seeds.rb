55020be0c82d85373f551487ab2c887893cdb689
Hayet Fellah <hayet.fellah145@gmail.com>
2023-02-21 15:53:02 +0100

    Add greetings table  and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Greeting.create(greeting_text: 'Hello')
+Greeting.create(greeting_text: 'What\'s up?')
+Greeting.create(greeting_text: 'How you doing ? ;)')
+Greeting.create(greeting_text: 'Hello, it\'s me.')
+Greeting.create(greeting_text: 'Hello from the databaaaaaaaaase')
